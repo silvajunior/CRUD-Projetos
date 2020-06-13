f192feba33746a614f9a0dba7ce13163f83caf52
everaldojunior <everaldodasilvajunior@gmail.com>
2020-06-12 21:56:43 -0300

    CRUD de Projetos, usando ExpressJs.

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+server.use((req,res,next)=>{
+  console.count('Count Request');
+  next();
+})
+
+/* Checa se o projetos existe.*/
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+/* Criar novo projeto */
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+/* Mostra todos os projetos */
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+/* Altera título do projeto */ 
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+/* Deletar projeto */
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+/* Adiciona Nova Tarefa */
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.listen(3500);
